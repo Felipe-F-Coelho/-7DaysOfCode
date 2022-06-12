0660be96d8b93308216907a90e24584a88e18d31
Felipe Coelho <Coelho.felipe.rj@gmail.com>
2022-06-12 19:42:42 -0300

    Adicionando requisição a API IMDb-API

--- src/main/java/ConsumerAPI.java
+++ src/main/java/ConsumerAPI.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class ConsumerAPI {
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.*;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Scanner;
+
+public class ConsumerAPI {
+    public static void main(String[] args){
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite a APIKey: ");
+        String apiKey = s.next();
+
+        HttpClient client = HttpClient.newBuilder()
+                .version(Version.HTTP_2)
+                .build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + apiKey))
+                .timeout(Duration.ofMinutes(1))
+                .header("Content-Type", "application/json")
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+
+
+    }
 }
