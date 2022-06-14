cebb2914e7879fafa24f7edc9e7000ba0ae74b9c
Felipe Coelho <Coelho.felipe.rj@gmail.com>
2022-06-13 23:47:24 -0300

    Extrair o título do filme e a URL da imagem a partir da resposta JSON para uma List.

--- src/main/java/ConsumerAPI.java
+++ src/main/java/ConsumerAPI.java
@@ -1,14 +1,21 @@
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpClient.*;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsumerAPI {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
+
+        List<String> titles = new ArrayList<>();
+        List<String> urlImages = new ArrayList<>();
 
         Scanner s = new Scanner(System.in);
 
@@ -18,16 +25,22 @@ public class ConsumerAPI {
         HttpClient client = HttpClient.newBuilder()
                 .version(Version.HTTP_2)
                 .build();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/" + apiKey))
-                .timeout(Duration.ofMinutes(1))
-                .header("Content-Type", "application/json")
                 .build();
-        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                .thenApply(HttpResponse::body)
-                .thenAccept(System.out::println)
-                .join();
 
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
 
+        JSONArray jsonArray = new JSONObject(json).getJSONArray("items");
+
+        for (int i = 0; i < jsonArray.length(); i++){
+            JSONObject jsonObject = new JSONObject(jsonArray.get(i).toString());
+            titles.add(jsonObject.get("title").toString());
+            urlImages.add(jsonObject.get("image").toString());
+        }
+        System.out.println(titles);
+        System.out.println(urlImages);
     }
 }
